9ce56c2d6df9bafb35d4ccf09028221aa06765a7
Hrithik <hrithik.kumar0210@gmail.com>
2025-06-14 13:48:01 +0530

    fix scroll issue

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import TodoList from "./components/Todo/TodoList";
 
 function App() {
   return (
-    <div className="min-h-screen bg-white text-black dark:bg-gray-900 dark:text-white transition-colors duration-300">
+    <div className="min-h-screen bg-white text-black dark:bg-gray-950 dark:text-white transition-colors border border-red-500 duration-300">
       <Header />
       <div className=" w-[23rem] md:w-[30rem] lg:w-[35rem] m-auto mt-10 flex flex-col gap-5">
         <TodoInput />
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="fixed bottom-0 font-semibold text-gray-700 dark:text-gray-300 bg-white inset-shadow-green-600 dark:bg-gray-900  w-full h-10 text-center transition-colors duration-300">
+    <div className="fixed bottom-0 font-semibold text-gray-700 dark:text-gray-300 bg-white inset-shadow-green-600 dark:bg-gray-950  w-svw pb-5 text-center transition-colors duration-300">
       Made with ❤️ by TaskLyst
     </div>
   );
